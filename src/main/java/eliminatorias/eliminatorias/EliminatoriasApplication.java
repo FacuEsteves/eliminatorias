0e7b1e726a1cefcebb39ebbeccc5712f7a89b9bf
Facundo Esteves <97993706+FacuEsteves@users.noreply.github.com>
2023-10-26 17:01:37 -0300

    Cambio de prueba

--- src/main/java/eliminatorias/eliminatorias/EliminatoriasApplication.java
+++ src/main/java/eliminatorias/eliminatorias/EliminatoriasApplication.java
@@ -11,5 +11,5 @@ public class EliminatoriasApplication {
     public static void main(final String[] args) {
         SpringApplication.run(EliminatoriasApplication.class, args);
     }
-
+    //Hola esto es un cambio para git
 }
