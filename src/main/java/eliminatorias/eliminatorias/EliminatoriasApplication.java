010a0bfed26c7be1496fea286c7208555c3ad801
Facundo Esteves <97993706+FacuEsteves@users.noreply.github.com>
2023-10-29 00:30:47 -0300

    Se agregaron clases faltantes de inicio de sesion. Cambio en relacion de arbitro-pais. Cambio en Textos.

--- src/main/java/eliminatorias/eliminatorias/EliminatoriasApplication.java
+++ src/main/java/eliminatorias/eliminatorias/EliminatoriasApplication.java
@@ -2,9 +2,10 @@ package eliminatorias.eliminatorias;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class EliminatoriasApplication {
 
     public static void main(final String[] args) {
--- src/main/java/eliminatorias/eliminatorias/domain/Arbitro.java
+++ src/main/java/eliminatorias/eliminatorias/domain/Arbitro.java
@@ -1,15 +1,7 @@
 package eliminatorias.eliminatorias.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
 import java.time.OffsetDateTime;
 import lombok.Getter;
 import lombok.Setter;
@@ -39,8 +31,8 @@ public class Arbitro {
     @Column(nullable = false)
     private String tipo;
 
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "pais_id", nullable = false, unique = true)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pais_id", nullable = false)
     private Pais pais;
 
     @CreatedDate
